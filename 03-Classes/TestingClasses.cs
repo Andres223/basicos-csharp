cfec6829a6748b57e9bd74b8bf14a7ee1a847cfd
Andrew <locogb26@gmail.com>
2026-07-22 05:43:24 -0500

    third commit

--- 03-Classes/TestingClasses.cs
+++ 03-Classes/TestingClasses.cs
@@ -1,10 +1,48 @@
-namespace HelloApp._03_Classes;
+namespace HelloApp;
 
 partial class Program
 {
     static void TestingClass()
     {
+        Vehicle toyota = new Vehicle();
+        toyota.Brand = "Toyota";
+        toyota.Model = "Corolla";
+        toyota.Year = 2021;
+        toyota.ShowInfo();
         
+        Vehicle honda = new Vehicle
+        {
+          Brand  = "Honda",
+          Model = "Civic",
+          Year = 2019
+        };
+        honda.ShowInfo();
+        
+        Vehicle renault = new Vehicle("Renault", "Duster", 2024);
+        renault.ShowInfo();
     }
 }
 
+class Vehicle
+{
+    public string? Brand { get; set; }
+    public string? Model { get; set; }
+    public int Year { get; set; }
+
+    public Vehicle(string brand, string model, int year)
+    {
+        Brand = brand;
+        Model = model;
+        Year = year;
+    }
+
+    public Vehicle()
+    {
+        
+    }
+    
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Este vehículo es un {Brand} {Model} del año {Year}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,20 +4,6 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        
-    }
-}
-
-class Vehicle
-{
-    // Propiedades
-    public string Brand { get; set; }
-    public string Model { get; set; }
-    public int Year { get; set; }
-    
-    // Métodos
-    public void ShowInfo()
-    {
-        Console.WriteLine($"Este vehículo es un {Brand} {Model} del año {Year}");
+        TestingClass();
     }
 }
