671baa36c8954a955791067bd411c404fad7793c
Andrew <locogb26@gmail.com>
2026-09-02 05:48:11 -0500

    visibility (properties, methods) 3

--- obj/Debug/net10.0/HelloApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/HelloApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81643f954842ca9d4447cdb66382739ead09ceb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afb4c0ff25916beb24cab1cd002f7932a21ed28c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
